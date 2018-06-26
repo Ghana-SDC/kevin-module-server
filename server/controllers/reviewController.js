534069e984cc8176b62077fb9df514162e6a22c3
Kevin Jacobs <frevin8r@gmail.com>
2018-06-25 21:09:50 -0700

    added post and put nosql models for reviews

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -2,8 +2,14 @@ const { ReviewModel } = require("../models/reviewModels.js")
 
 const ReviewCtrl = {
   get: (req, res) => {
-    let params = { productId: req.params.search };
-    ReviewModel.get(params, (err, data) => {
+    ReviewModel.get(req.params.id , (err, data) => {
+      (err) && res.send(err).status(400);
+      res.send(data).status(200);
+    })
+  },
+
+  getAll: (req, res) => {
+    ReviewModel.getAll((err, data) => {
       (err) && res.send(err).status(400);
       res.send(data).status(200);
     })
@@ -17,7 +23,8 @@ const ReviewCtrl = {
       review: req.body.review,
       helpful_count: Number(req.body.helpful_count),
       productId: Number(req.body.productId),
-      verified:  req.body.verified === 'true' ? true : false
+      verified:  req.body.verified === 'true' ? true : false,
+      updatedAt: new Date(Date.now()).toISOString()
     };
     ReviewModel.post(body, (err, data) => {
       (err) && res.send(err).status(400);
@@ -26,19 +33,17 @@ const ReviewCtrl = {
   },
 
   put: (req, res) => {
-    let body = { 
-      id: req.params.id, 
-      customer_name: req.body.customer_name,
-      rating: req.body.rating,
-      date: req.body.date,
-      title: req.body.title,
-      review: req.body.review,
-      helpful_count: req.body.helpful_count,
-      productId: req.body.productId,
-      verified: req.body.verified ? (req.body.verified === 'true' ? true : false) : undefined
-    };
-    ReviewModel.put (body, (err, data) => {
-      console.log(data);
+    let id =  req.params.id;
+    let body = {};
+    (req.body.customer_name) && (body.customer_name = req.body.customer_name);
+    (req.body.rating) && (body.rating = req.body.rating);
+    (req.body.date) && (body.date = req.body.date);
+    (req.body.title) && (body.title = req.body.title);
+    (req.body.review) && (body.review = req.body.review);
+    (req.body.helpful_count) && (body.helpful_count = req.body.helpful_count);
+    (req.body.productId) && (body.productId = req.body.productId);
+    (req.body.verified) && (body.verified = req.body.verified);
+    ReviewModel.put (id, body, (err, data) => {
       (err) && res.send(err).status(400);
       res.send(data).status(200);
     })
